a3f3dc7af0d9d3b7aa98016d71071fe1c21dee21
Carlos Eduardo <anjoazrael10@gmail.com>
2021-03-27 13:01:16 -0300

    add cors routees

--- src/router.js
+++ src/router.js
@@ -1,11 +1,18 @@
 const routes = require(`express`).Router()
 const UserController = require ('../app/controller/UserController')
 const LoginController  =  require('../app/controller/LoginController')
+var cors = require('cors')
+ 
+var corsOptions = {
+    origin: 'http://example.com',
+    optionsSuccessStatus: 200 
+  }
+   
 
+// routes.get('/users',cors(corsOptions),  UserController.index)
+routes.get('/users',cors(),  UserController.index)
+routes.post('/users',cors(), UserController.store)
 
-routes.get('/users', UserController.index)
-routes.post('/users', UserController.store)
-
-routes.post('/login',LoginController.auth)
+routes.post('/login',cors() ,LoginController.auth)
 
 module.exports = routes
